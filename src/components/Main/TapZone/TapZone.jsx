8010c3413de591f5b9d62a064e9e14d982fa678f
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-07-13 16:50:03 +0300

    numbers add

--- src/components/Main/TapZone/TapZone.jsx
+++ src/components/Main/TapZone/TapZone.jsx
@@ -11,25 +11,34 @@ const TapZone = () => {
 			tg.HapticFeedback.impactOccurred('light')
 		}
 
-		const { clientX, clientY } = event
-		const rect = event.currentTarget.getBoundingClientRect()
+		const newTaps = []
 
-		const newTap = {
-			id: Date.now(),
-			x: clientX - rect.left,
-			y: clientY - rect.top,
+		// Loop through touches (fingers) on the screen
+		for (let i = 0; i < event.touches.length; i++) {
+			const { clientX, clientY } = event.touches[i]
+			const rect = event.currentTarget.getBoundingClientRect()
+
+			const newTap = {
+				id: Date.now() + i, // Ensure unique ID
+				x: clientX - rect.left,
+				y: clientY - rect.top,
+			}
+
+			newTaps.push(newTap)
 		}
 
-		setTaps(prevTaps => [...prevTaps, newTap])
+		setTaps(prevTaps => [...prevTaps, ...newTaps])
 
-		// Remove the tap after the animation ends (1 second)
+		// Remove the taps after the animation ends (1 second)
 		setTimeout(() => {
-			setTaps(prevTaps => prevTaps.filter(tap => tap.id !== newTap.id))
+			setTaps(prevTaps =>
+				prevTaps.filter(tap => !newTaps.some(newTap => newTap.id === tap.id))
+			)
 		}, 1000)
 	}
 
 	return (
-		<div onClick={feedback} className={style.tap_zone}>
+		<div onTouchStart={feedback} className={style.tap_zone}>
 			<img className={style.tap_img} src='/tap.jpeg' alt='tap' />
 			{taps.map(tap => (
 				<span
