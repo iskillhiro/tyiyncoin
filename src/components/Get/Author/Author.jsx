9676b1d9a86083a718d87c482fdb935e406b3baa
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-07-13 18:14:06 +0300

    numbers update

--- src/components/Get/Author/Author.jsx
+++ src/components/Get/Author/Author.jsx
@@ -4,7 +4,7 @@ function Author() {
 	return (
 		<div className={style.author}>
 			<img className={style.author_img} src='/tap.jpeg' alt='auhtor' />
-			<h2 className={style.author_name}>@rustamovrahman</h2>
+			<h2 className={style.author_name}>@tc3050100</h2>
 			<Link to={'https://t.me/tc3050100'} className={style.gradient_btn}>
 				Связаться
 			</Link>
