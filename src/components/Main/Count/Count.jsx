1d2a77ba6d20db9edcf4c417bf68857d8afa0dcc
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-07-19 00:14:20 +0300

    bonus added, taps added, energy added

--- src/components/Main/Count/Count.jsx
+++ src/components/Main/Count/Count.jsx
@@ -1,10 +1,14 @@
 import style from './Count.module.css'
+
 const Count = ({ currentCoins }) => {
+	// Round the currentCoins to 2 decimal places and convert to a number
+	const roundedCoins = Number(currentCoins.toFixed(2))
+
 	return (
 		<div className={style.counter}>
 			<div className={style.count}>
 				<img src='/coin.svg' alt='' />
-				{currentCoins}
+				{roundedCoins}
 			</div>
 		</div>
 	)
