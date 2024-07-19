5c12e21e357c6bf13d6f6de61551358e36284750
iskillhiro <155888748+iskillhiro@users.noreply.github.com>
2024-07-19 12:54:07 +0300

    frontend

--- src/utils/axiosDB.js
+++ src/utils/axiosDB.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosDB = axios.create({
-	baseURL: 'http://localhost:5000/api',
+	baseURL: 'https://pinocchionode-1.onrender.com/api',
 })
 
 export default axiosDB
